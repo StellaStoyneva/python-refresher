cd4a64fccc3cf7e448ec0ccda314e1b199c97cd2
Stella Stoyneva <stella.stoyneva@gmail.com>
2021-10-13 22:38:41 +0300

    feat: refactor SendinBlue class/opt/homebrew/bin/python3

--- 03-send-email-with-sendinblue/Email.py
+++ 03-send-email-with-sendinblue/Email.py
@@ -1,15 +1,19 @@
-from SendInBlueMixIn import SendInBlueMixIn
+from SendInBlue import SendInBlue
 from HTMLMixIn import HTMLMixIn
 from constants import HTML_PATH, TXT_PATH, SENDINBLUE_API_KEY
-class Email(SendInBlueMixIn, HTMLMixIn):
+class Email(HTMLMixIn):
     def __init__(self):
-        self.content = ''
+        self.__html_content = None
+        self.__plain_text_content = None
     
     def get_content(self):
         return self._content
       
-    def set_content(self, x):
-        self._content = x
+    def set_html_content(self, content):
+        self._content = content
+
+    def set_plain_text_content(self, content):
+        self.__plain_text_content = content
 
     def validate_content_type(self, content_type):
         if(content_type not in ['html', 'text']):
@@ -27,16 +31,17 @@ class Email(SendInBlueMixIn, HTMLMixIn):
         source_code = self.get_content_source_code(content_type)
         if content_type =='html': 
             HTMLMixIn.validate_html_content(source_code) 
-        self.set_content(source_code)
+            self.set_html_content(source_code)
+            return
+        self.set_plain_text_content(source_code)
 
-    def send_email(self, content_type):
+    def send_email(self, content_type, sendInBlue):
         self.set_email_content(content_type)
-        content = self.get_content()
-        html_content = content if content_type == 'html' else None
-        plain_text_content = None if content_type == 'html' else content
-        SendInBlueMixIn.send_email_via_sendinblue(html_content, plain_text_content, "stella.stoyneva@mentormate.com", SENDINBLUE_API_KEY)
+        sendInBlue.configure(SENDINBLUE_API_KEY)
+        sendInBlue.send_email_via_sendinblue(self.__html_content, self.__plain_text_content)
 
 
 email = Email()
+sendInBlue = SendInBlue()
 
-email.send_email('text')
+email.send_email('text', sendInBlue)
